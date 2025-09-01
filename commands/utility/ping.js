9128a0470e3f6d672d30de52d60e897185f7c54d
pepe3 <pepe3@users.noreply.github.com>
2025-09-01 22:11:24 +0300

    refactor app

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -4,7 +4,12 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
+        
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        await interaction.reply({
+            content: 'Pong!',
+            components: [],
+            withResponse: true,
+        });
     },
 };
