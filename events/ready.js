9673eca201b44073d5e0db1301c10c85021fbaf1
pepe3 <pepe3@users.noreply.github.com>
2025-09-16 17:42:57 +0300

    run tasks with debug

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,13 @@
 const { Events } = require('discord.js');
+const cron = require('node-cron');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        cron.schedule('* * * * *', () => {
+            console.log('running a task every minute');
+        });
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
 };
