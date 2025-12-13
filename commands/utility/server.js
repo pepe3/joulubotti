3eeac6c7db23d8cdcc1ef46e984cb279e5c592b4
pepe3 <pepe3@users.noreply.github.com>
2025-12-13 08:54:53 +0200

    add feature to fetch news from yle api

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,11 +1,48 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MediaGalleryBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.'),
-	async execute(interaction) {
-		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members. Server runs on Node version ${process.versions.node.split('.').map(Number)}`);
+	async execute(interaction, client) {
+
+		const media = new MediaGalleryBuilder()
+			.addItems([
+				{
+					media: {
+						url: 'https://external.api.yle.fi/v1/teletext/images/100/1.png?app_id=e9b08e2d2dcca4dc62d2f1e3ebfd456b&app_key=adace51a'
+					}
+				},
+				{
+					media: {
+						url: 'https://external.api.yle.fi/v1/teletext/images/101/1.png?app_id=e9b08e2d2dcca4dc62d2f1e3ebfd456b&app_key=adace51a'
+					}
+				},
+				{
+					media: {
+						url: 'https://external.api.yle.fi/v1/teletext/images/102/1.png?app_id=e9b08e2d2dcca4dc62d2f1e3ebfd456b&app_key=adace51a'
+					}
+				},
+				{
+					media: {
+						url: 'https://external.api.yle.fi/v1/teletext/images/103/1.png?app_id=e9b08e2d2dcca4dc62d2f1e3ebfd456b&app_key=adace51a'
+					}
+				},
+				{
+					media: {
+						url: 'https://external.api.yle.fi/v1/teletext/images/200/1.png?app_id=e9b08e2d2dcca4dc62d2f1e3ebfd456b&app_key=adace51a'
+					}
+				},
+				{
+					media: {
+						url: 'https://external.api.yle.fi/v1/teletext/images/201/1.png?app_id=e9b08e2d2dcca4dc62d2f1e3ebfd456b&app_key=adace51a'
+					}
+				},
+			]);
+
+		await interaction.reply({
+			flags: MessageFlags.IsComponentsV2,
+			components: [media],
+		})
 	},
 };
--- events/ready.js
+++ events/ready.js
@@ -7,45 +7,36 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        
-        cron.schedule('0 0 8 * * *', () => {
 
-            console.log('running a task every day at 8:00');
-            // Hae kanava, johon haluat lähettää viestin
-            const channel = client.channels.cache.get(process.env.TEXT_CHANNEL_ID);
+        // const channel = client.channels.cache.get(process.env.TEXT_CHANNEL_ID);
+        // cron.schedule('0 0 8 * * *', () => {
 
-            // Kutsu komentoa ja lähetä viesti kanavalle
-            joulu.execute({
-                reply: (message) => {
-                    if (channel) {
-                        channel.send(message);
-                    } else {
-                        console.log('Kanavaa ei löytynyt');
-                    }
-                }
-            });
-        });
+        //     console.log('running a task every day at 8:00');
+        //     // Hae kanava, johon haluat lähettää viestin
 
-        cron.schedule('0 0 9 * * *', () => {
+        //     // Kutsu komentoa ja lähetä viesti kanavalle
+        //     joulu.execute({
+        //         reply: (message) => {
+        //             if (channel) {
+        //                 channel.send(message);
+        //             } else {
+        //                 console.log('Kanavaa ei löytynyt');
+        //             }
+        //         }
+        //     });
+        // });
 
-            console.log('running a task every day at 9:00');
-            
-            const channel = client.channels.cache.get(process.env.TEXT_CHANNEL_ID);
+        // Kutsu komentoa ja lähetä viesti kanavalle
+        // vitsit.execute({
+        //     reply: (message) => {
+        //         if (channel) {
+        //             channel.send(message);
+        //         } else {
+        //             console.log('Kanavaa ei löytynyt');
+        //         }
+        //     }
+        // });
 
-            // Kutsu komentoa ja lähetä viesti kanavalle
-            vitsit.execute({
-                reply: (message) => {
-                    if (channel) {
-                        channel.send(message);
-                    } else {
-                        console.log('Kanavaa ei löytynyt');
-                    }
-                }
-            });
-        }, {
-            name: 'Joululaskuri',
-            timezone: 'Europe/Helsinki',
-        });
         console.log(`Ready! Logged in as ${client.user.tag}`);
     }
 };
